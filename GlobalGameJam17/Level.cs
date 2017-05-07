a20e445547a08edc7e585aa68c1ae02859b4ec9b
Victor <baumbach.victor@gmail.com>
2017-05-07 18:28:42 +0100

    Added the backbone of a level system.

---
+++ GlobalGameJam17/Level.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace GlobalGameJam17
+{
+    class Level
+    {
+        List<Mexican> mexicans = new List<Mexican>();
+        List<Keys> solution;
+
+        public Level(List<Keys> problem, List<Keys> _solution)
+        {
+            solution = _solution;
+            foreach(Keys key in problem)
+            {
+                Mexican newMexican = new Mexican(key);
+                mexicans.Add(newMexican);
+            }
+        }
+    }
+}
---
+++ GlobalGameJam17/Mexican.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace GlobalGameJam17
+{
+    class Mexican
+    {
+        Keys key;
+
+        public Mexican (Keys _key)
+        {
+            key = _key;
+        }
+    }
+}
