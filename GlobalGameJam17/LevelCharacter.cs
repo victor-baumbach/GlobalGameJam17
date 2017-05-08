d75de7cb63a6ce3efb2199b06991c5337e585e85
Victor <baumbach.victor@gmail.com>
2017-05-08 06:42:29 +0100

    minor change that I might regret while I merge.

--- GlobalGameJam17/LevelCharacter.cs
+++ GlobalGameJam17/LevelCharacter.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
 
 namespace GlobalGameJam17
 {
     class LevelCharacter
     {
         char key;
+        Vector2 position = new Vector2();
 
         public LevelCharacter (char _key)
         {
